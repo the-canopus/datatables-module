0b31ffae3567c54180a8c74b3696c46ce68adca8
Soheil Rahmat <soheilrt314@gmail.com>
2019-11-30 16:04:50 +0330

    Apply stylefix

--- Providers/DatatablesServiceProvider.php
+++ Providers/DatatablesServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace Modules\Datatables\Providers;
 
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Factory;
+use Illuminate\Support\ServiceProvider;
 
 class DatatablesServiceProvider extends ServiceProvider
 {
